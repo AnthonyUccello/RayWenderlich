1c4d9dde9cf57dd082e03f9cf172698df57432f7
Anthony Uccello <anthonyuccello@gmail.com>
2016-05-29 03:05:01 -0400

    :star: Add ammo count with Get() syntax.

---
+++ Ready Aim FIRE/Assets/GameUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+  public Text ammoText;
+
+  public vp_PlayerEventHandler playerEventHandler;
+	
+	// Update is called once per frame
+	void Update () {
+    ammoText.text = "Ammo:" + playerEventHandler.CurrentWeaponAmmoCount.Get();
+  }
+}
