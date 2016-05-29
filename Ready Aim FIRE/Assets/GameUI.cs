c2b42e806de371fef16620eac78c825b7f12facb
Anthony Uccello <anthonyuccello@gmail.com>
2016-05-29 18:13:55 -0400

    :star::bug: Adds all weapons with projectiles and fixes bug with weaponShooter script not appearing in inspector

--- Ready Aim FIRE/Assets/GameUI.cs
+++ Ready Aim FIRE/Assets/GameUI.cs
@@ -5,11 +5,20 @@ using UnityEngine.UI;
 public class GameUI : MonoBehaviour {
 
   public Text ammoText;
+  public GameObject sniperZoom;
 
   public vp_PlayerEventHandler playerEventHandler;
 	
 	// Update is called once per frame
 	void Update () {
     ammoText.text = "Ammo:" + playerEventHandler.CurrentWeaponAmmoCount.Get();
+  }
+
+  public void ShowSniperZoom(){
+    sniperZoom.GetComponent<Image>().enabled = true;
+  }
+
+  public void HideSniperZoom(){
+    sniperZoom.GetComponent<Image>().enabled = false;
   }
 }
---
+++ Ready Aim FIRE/Assets/Sniper.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sniper : MonoBehaviour {
+
+  public vp_FPPlayerEventHandler playerEventHandler;
+  public GameUI gameUI;
+  public Camera camera;
+
+  private bool isZooming = false;
+  private bool hasZoomed = false;
+	
+	void Update () {
+	  if(playerEventHandler.Zoom.Active && !isZooming) {
+      isZooming = true;
+      StartCoroutine("ZoomSniper");
+    } else if (!playerEventHandler.Zoom.Active) {
+      gameUI.HideSniperZoom();
+      isZooming = false;
+      hasZoomed = false;
+    }
+
+    if (hasZoomed) {
+      camera.fieldOfView = 6;
+    }
+	}
+
+  IEnumerator ZoomSniper(){
+    yield return new WaitForSeconds(0.15f);
+    gameUI.ShowSniperZoom();
+    hasZoomed = true;
+  }
+}
