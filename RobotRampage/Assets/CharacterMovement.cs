619bce39d0a47ddb6febb11b2af364e71109cda8
Anthony Uccello <anthonyuccello@gmail.com>
2016-06-02 01:37:04 -0400

    :star: Add robot rampage project with movement scripts

---
+++ RobotRampage/Assets/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+  public float speed = 2;
+  public float turnSpeed = 100;
+
+  void Update()
+  {
+    if (Input.GetKey("w"))
+    {
+      transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    } else if (Input.GetKey("a"))
+    {
+      transform.Translate(Vector3.left * speed * Time.deltaTime);
+    } else if (Input.GetKey("s"))
+    {
+      transform.Translate(Vector3.right * speed * Time.deltaTime);
+    } else if (Input.GetKey("d"))
+    {
+      transform.Translate(Vector3.back * speed * Time.deltaTime);
+    }
+  }
+
+}
